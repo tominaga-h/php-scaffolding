ea66e827f0e9a9c569db2333fb2bbf4600a60817
TOMINAGA HAYATO <tominaga_h@mad2007.co.jp>
2025-04-19 00:15:53 +0900

    EditProcessTest: テスト実装

--- src/PhpScff/Process/EditProcess.php
+++ src/PhpScff/Process/EditProcess.php
@@ -12,6 +12,17 @@ class EditProcess
 	public function __construct()
 	{
 		$this->editor = $_SERVER['EDITOR'] ?? 'vim';
+		$this->checkEditor();
+	}
+
+	/**
+	 * エディタが存在するかどうかを確認する
+	 *
+	 * @return void
+	 * @throws ProcessException エディタが存在しない場合
+	 */
+	public function checkEditor(): void
+	{
 		if (!$this->editorExists()) {
 			$msg = 'Editor "' . $this->editor . '" is not found.' . PHP_EOL;
 			$msg .= 'Please set your editor to the `EDITOR` environment variable.';
@@ -19,9 +30,25 @@ class EditProcess
 		}
 	}
 
-	private function editorExists(): bool
+	/**
+	 * Processオブジェクトを作成する
+	 *
+	 * @param array $command コマンドとその引数の配列
+	 * @return Process
+	 */
+	public function createProcess(array $command): Process
 	{
-		$proc = new Process(['which', $this->editor]);
+		return new Process($command);
+	}
+
+	/**
+	 * エディタが存在するかどうかを確認する
+	 *
+	 * @return bool エディタが存在する場合は true, 存在しない場合は false
+	 */
+	public function editorExists(): bool
+	{
+		$proc = $this->createProcess(['which', $this->editor]);
 		$proc->run();
 		return $proc->isSuccessful();
 	}
@@ -34,7 +61,7 @@ class EditProcess
 	 */
 	public function edit(string $path): bool
 	{
-		$proc = new Process([$this->editor, $path]);
+		$proc = $this->createProcess([$this->editor, $path]);
 		$proc->setTty(true);
 		$proc->run();
 		return $proc->isSuccessful();
---
+++ tests/Process/EditProcessTest.php
@@ -0,0 +1,135 @@
+<?php
+
+namespace Tests\Process;
+
+use PHPUnit\Framework\TestCase;
+use PHPUnit\Framework\MockObject\MockObject;
+use Hytmng\PhpScff\Process\EditProcess;
+use Hytmng\PhpScff\Exception\ProcessException;
+use Symfony\Component\Process\Process;
+
+class EditProcessTest extends TestCase
+{
+    private ?string $originalEditor;
+    private Process&MockObject $processMock;
+    private EditProcess&MockObject $editProcess;
+
+    protected function setUp(): void
+    {
+        // EDITORの環境変数をバックアップ
+        $this->originalEditor = $_SERVER['EDITOR'] ?? null;
+
+        // Processクラスのモックを作成
+        $this->processMock = $this->getMockBuilder(Process::class)
+            ->disableOriginalConstructor()
+            ->getMock();
+
+        // EditProcessクラスのモックを作成
+        $this->editProcess = $this->getMockBuilder(EditProcess::class)
+            ->disableOriginalConstructor()
+            ->onlyMethods(['createProcess'])
+            ->getMock();
+
+        $this->editProcess->method('createProcess')
+            ->willReturn($this->processMock);
+    }
+
+    protected function tearDown(): void
+    {
+        // EDITORの環境変数を復元
+        if ($this->originalEditor === null) {
+            unset($_SERVER['EDITOR']);
+        } else {
+            $_SERVER['EDITOR'] = $this->originalEditor;
+        }
+    }
+
+    public function testConstructWithValidEditor(): void
+    {
+        $_SERVER['EDITOR'] = 'vim';
+
+        $this->processMock->expects($this->once())
+            ->method('run');
+        $this->processMock->expects($this->once())
+            ->method('isSuccessful')
+            ->willReturn(true);
+
+        $this->editProcess->__construct();
+        $this->assertInstanceOf(EditProcess::class, $this->editProcess);
+    }
+
+    public function testConstructWithInvalidEditor(): void
+    {
+        $_SERVER['EDITOR'] = 'non_existent_editor';
+
+        $this->processMock->expects($this->once())
+            ->method('run');
+        $this->processMock->expects($this->once())
+            ->method('isSuccessful')
+            ->willReturn(false);
+
+        $this->expectException(ProcessException::class);
+        $this->expectExceptionMessage('Editor "non_existent_editor" is not found.' . PHP_EOL . 'Please set your editor to the `EDITOR` environment variable.');
+
+        $this->editProcess->__construct();
+    }
+
+    public function testConstructWithoutEditor(): void
+    {
+        unset($_SERVER['EDITOR']);
+
+        $this->processMock->expects($this->once())
+            ->method('run');
+        $this->processMock->expects($this->once())
+            ->method('isSuccessful')
+            ->willReturn(true);
+
+        $this->editProcess->__construct();
+        $this->assertInstanceOf(EditProcess::class, $this->editProcess);
+    }
+
+    public function testEdit(): void
+    {
+        $_SERVER['EDITOR'] = 'vim';
+        $filePath = '/path/to/file';
+
+        $this->processMock->expects($this->exactly(2))
+            ->method('run');
+        $this->processMock->expects($this->exactly(2))
+            ->method('isSuccessful')
+            ->willReturn(true);
+        $this->processMock->expects($this->once())
+            ->method('setTty')
+            ->with(true);
+
+        $this->editProcess->__construct();
+        $result = $this->editProcess->edit($filePath);
+        $this->assertTrue($result);
+    }
+
+    public function testEditorExists(): void
+    {
+        $_SERVER['EDITOR'] = 'vim';
+
+        $this->processMock->expects($this->exactly(2))
+            ->method('run');
+        $this->processMock->expects($this->exactly(2))
+            ->method('isSuccessful')
+            ->willReturn(true);
+
+        $this->editProcess->__construct();
+        $result = $this->editProcess->editorExists();
+        $this->assertTrue($result);
+    }
+
+    public function testCreateProcess(): void
+    {
+        // EditProcessのモックを使用せずに実際のインスタンスを作成
+        $editProcess = new EditProcess();
+        $command = ['test', 'command'];
+
+        $process = $editProcess->createProcess($command);
+
+        $this->assertInstanceOf(Process::class, $process);
+    }
+}
